9e7202398a7e5d247a82117985d592394af38c9c
mymzbe <aizuddin.zali@gmail.com>
2017-07-08 22:39:40 +0800

    basic example of get each pod endpoints

--- get_data.py
+++ get_data.py
@@ -2,7 +2,23 @@
 
 import requests
 
-url = 'https://console.devopshumans.com:8443/oapi/v1'
-headers = {'Authorization': 'Bearer GEG7SjadJVaYU2lU1q-4pLb-6XKmCuexG0BweY8lbiI'}
+url = 'https://console.devopshumans.com:8443/api/v1/endpoints'
+headers = {'Authorization': 'Bearer oc whoami -t token'}
 
-requests.get(url, headers=headers, verify=False)
+res = requests.get(url, headers=headers, verify=False)
+data_json = res.json()
+
+items = data_json['items']
+
+for item in items:
+    if len(item['subsets']) > 0:
+        subdata = item['subsets']
+        for data in subdata:
+            print "*" * 50
+            ip = data['addresses'][0]['ip']
+            if data['addresses'][0].has_key('targetRef'):
+                targetRef = data['addresses'][0]['targetRef']
+                print "Name = " + targetRef['name']
+                print "Kind = " + targetRef['kind']
+                print "Namespace = " + targetRef['namespace']
+                print "Local IP = " + ip
